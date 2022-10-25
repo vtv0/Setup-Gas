083640f266eb34d328a33caa6ff3af2d30f12bbf
vtv <vbc14365@gmail.com>
2022-10-25 17:25:48 +0700

    10 for API

--- Gas/Screen/Delivery List/Navigation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Navigation.swift
-//  Gas
-//
-//  Created by Vuong The Vu on 21/10/2022.
-//
-
-import Foundation
-
---
+++ Gas/Screen/Delivery List/ParseJsontoObject.swift
@@ -0,0 +1,8 @@
+//
+//  ParseJsontoObject.swift
+//  Gas
+//
+//  Created by Vuong The Vu on 25/10/2022.
+//
+
+import Foundation
