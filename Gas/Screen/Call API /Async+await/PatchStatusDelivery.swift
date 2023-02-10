8a3ff5759fc67545082dc386b9baefff5da2f4d5
vtv <vbc14365@gmail.com>
2023-02-10 18:08:26 +0700

    94 PatchStatusDelivery

---
+++ Gas/Screen/Call API /Async+await/PatchStatusDelivery.swift
@@ -0,0 +1,18 @@
+//
+//  PatchStatusDelivery.swift
+//  Gas
+//
+//  Created by Vuong The Vu on 10/02/2023.
+//
+
+import Foundation
+
+class PatchStatusDelivery {
+    
+    
+    let urlPatch: String = "https://am-stg-iw01j.kiiapps.com/am/api/assets/th.bc65e7a00022-8048-ce11-cebd-37c4fdc4/properties"
+    
+    func patchStatusDelivery_Async_Await(iassetID: String) async throws {
+        
+    }
+}
